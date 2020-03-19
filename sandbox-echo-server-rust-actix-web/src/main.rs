6c1f2595c4b4524b7641d4c37967644ba44f0eb7
m4ik7i <m4ik7i@gmail.com>
2020-03-20 00:05:08 +0900

    imp: improve sandbox-echo-server-rust-actix-web

--- sandbox-echo-server-rust-actix-web/src/main.rs
+++ sandbox-echo-server-rust-actix-web/src/main.rs
@@ -1,13 +1,13 @@
 use actix_web::{web, App, HttpServer, Responder};
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize)]
-struct Query {
-    message: String,
+#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
+struct Message {
+    msg: String,
 }
 
-fn index(query: web::Query<Query>) -> impl Responder {
-    format!("{}", query.message)
+fn index(query: web::Query<Message>) -> impl Responder {
+    format!("{}", query.msg)
 }
 
 #[rustfmt::skip]
@@ -16,6 +16,6 @@ fn main() -> std::io::Result<()> {
         App::new()
             .service(web::resource("/").route(web::get().to(index)))
     })
-    .bind("localhost:12345")?
+    .bind("127.0.0.1:8080")?
     .run()
 }
